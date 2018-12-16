5846c7ec6358e5c430efe47ccc9d19b336786027
collins <chigoziecollins434@gmail.com>
2018-12-16 15:35:20 +0100

    Added the components folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const kim = true;
+    let mat;
+    if (kim) {
+      mat = <h4>You are welcome to the lords house</h4>;
+    } else {
+      mat = <h3>You are not welcomed</h3>;
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>React App</h1>
+        {mat}
+        {/*  <p>
+          {kim ? (
+            <h4>You are welcome to the lords house</h4>
+          ) : (
+            <h3>You are not welcomed</h3>
+          )}
+        </p> */}
       </div>
     );
   }
